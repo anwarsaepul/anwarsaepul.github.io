42af3c0efd4d8ebec3e315e0a432c5585087d94a
Saepul Anwar <saepulanwarxyz@gmail.com>
2020-10-19 16:09:33 +0700

    Smoot Scrolling & Menambahkan parallax effect

--- js/script.js
+++ js/script.js
@@ -11,10 +11,16 @@ $('.page-scroll').on('click', function(event) {
 });
 
 // Parallax
+// About
+$(window).on('load', function(){
+	$('.pKiri').addClass('pTampil');
+	$('.pKanan').addClass('pTampil');
+});
+
 $(window).scroll(function() {
 	let wScroll = $(this).scrollTop();
-	console.log(wScroll);
-
+	
+	// Jumbotron
 	$('.jumbotron img').css({
 		'transform'	: 'translate(0px, '+ wScroll/4 +'%)'
 	});
@@ -26,4 +32,21 @@ $(window).scroll(function() {
 	$('.jumbotron p').css({
 		'transform'	: 'translate(0px, '+ wScroll/1.2 +'%)'
 	});
+
+
+	// Portofolio
+	if (wScroll > $('.portofolio').offset().top - 200) {
+		
+		$('.portofolio .img-thumbnail').each(function(i){
+			setTimeout(function(){
+				$('.portofolio .img-thumbnail').eq(i).addClass('tampil')
+
+			}, 300 * i+1);
+		});
+	}
+
+	if (wScroll < $('.jumbotron').offset().top) {
+		$('.portofolio .img-thumbnail').removeClass('tampil')
+	}
+
 })
