bfa90ce6d581b49931cc2cdfb1bd9096c3fce584
Saepul Anwar <saepulanwarxyz@gmail.com>
2020-10-19 14:11:54 +0700

    Smoot Scrolling & Menambahkan parallax effect

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 // event pada saat link di klik
-$('.page-scroll').on('click', function() {
-
-	// Ambil isi href
-	var tujuan = $(this).attr('href');
-	
-	// Menangkap element yg di klik
-	var elementTujuan = $(tujuan);
-// console.log(elementTujuan.offset().top);
-// $(elementTujuan)
-	// console.log($('body').scrollTop(elementTujuan.offset().top));
-	// console.log($('body').scrollTop(400));
-	console.log(tujuan);
-	// console.log(elementTujuan.offset().top);
-	// 	
-	$(tujuan).click(function() {
-		$(tujuan).slideDown(3000);
-	})
-	// .animate({
-	// 	scrollTop: elementTujuan.offset().top - 50
-	// }, 1250, 'swing');
-
-	// e.preventDefault();
-
-
-
+$('.page-scroll').on('click', function(event) {
+	if (this.hash !=='') {
+		event.preventDefault();
+		const hash = this.hash;
+		$('html, body').animate({
+			scrollTop: $(hash).offset().top - 60
+				}, 1000, 'easeInOutExpo'
+		);
+	}
 });
+
+// Parallax
+$(window).scroll(function() {
+	let wScroll = $(this).scrollTop();
+	console.log(wScroll);
+
+	$('.jumbotron img').css({
+		'transform'	: 'translate(0px, '+ wScroll/4 +'%)'
+	});
+
+	$('.jumbotron h1').css({
+		'transform'	: 'translate(0px, '+ wScroll/2 +'%)'
+	});
+
+	$('.jumbotron p').css({
+		'transform'	: 'translate(0px, '+ wScroll/1.2 +'%)'
+	});
+})
